cb34c15b255d43c214831e23cebdbe03c51ab8ff
Adam Brinckman <abrinckm@gmail.com>
2021-04-15 12:22:22 -0400

    grab settings from env

--- libsimba/settings.py
+++ libsimba/settings.py
@@ -9,15 +9,15 @@ PROJECT_PATH = dirname(__file__)
 #SCAAS API & Auth settings
 
 # Options: client_credentials (Default), pkce, password
-AUTH_FLOW = "client_credentials" 
+AUTH_FLOW = os.getenv('AUTH_FLOW', "pkce")
 
 # [Optional] CLIENT_SECRET is only needed if using the client credentials flow.
 CLIENT_SECRET = os.getenv('CLIENT_SECRET', '<set in local settings>')
 CLIENT_ID = os.getenv('CLIENT_ID', '11cbd388-342e-4d19-a6ca-97aa56deaad3')
-SCOPE = "api://{}/scaas.access".format(CLIENT_ID)
+SCOPE = os.getenv('SCOPE', "api://{}/scaas.access".format(CLIENT_ID))
 
 BASE_AUTH_URL = "https://login.microsoftonline.com"
-TENANT_ID = "<set in local settings>"
+TENANT_ID = os.getenv('TENANT_ID', '<set in local settings>')
 AUTH_ENDPOINT = "/{}/oauth2/v2.0/".format(TENANT_ID)
 
 BASE_API_URL = "https://api.sep.dev.simbachain.com"
