b94dd8d4ade7941b09e1bd3db7db91874537a26d
bpbirch <brendanpbirch@gmail.com>
2021-05-27 19:29:47 -0700

    structs are now included as classes. type hinting includes custom classes / structs.

--- tests/app_md_3_contract.py
+++ tests/app_md_3_contract.py
@@ -11,6 +11,34 @@ class AssemblyBom:
         self.simba = Simba(self.base_api_url)
         self.simba_contract = self.simba.get_contract(self.app_name, self.contract_name)
     
+    class Part:
+        def __init__(self, __Part: str = ''):
+            self.__Part=__Part
+    
+    class Assembly:
+        def __init__(self, __Assembly: str = ''):
+            self.__Assembly=__Assembly
+    
+    class Supplier:
+        def __init__(self, __Supplier: str = ''):
+            self.__Supplier=__Supplier
+    
+    class StockItem:
+        def __init__(self, __StockItem: str = '', part: libsimba.AssemblyBom.Part = None, unitOfIssue: str = '', SMRCode: str = '', NSN: str = '', partNumber: str = '', UOC: str = ''):
+            self.__StockItem=__StockItem
+            self.part=part
+            self.unitOfIssue=unitOfIssue
+            self.SMRCode=SMRCode
+            self.NSN=NSN
+            self.partNumber=partNumber
+            self.UOC=UOC
+    
+    class AssemblyPart:
+        def __init__(self, __AssemblyPart: str = '', part: libsimba.AssemblyBom.Part = None, quantity: str = ''):
+            self.__AssemblyPart=__AssemblyPart
+            self.part=part
+            self.quantity=quantity
+    
     def get_transactions(self, opts: Optional[dict] = None):
         return self.simba_contract.get_transactions(opts=opts)
     
@@ -20,7 +48,7 @@ class AssemblyBom:
     def get_transaction_statuses(self, txn_hashes: List[str] = None, opts: Optional[dict] = None):
         return self.simba_contract.get_transaction_statuses(txn_hashes, opts)
 
-    def mbomStock(self, stockItem: dict, specifiedSupplier: dict, SOH: str, remanufacturedCost: str, totalCost: str, comment: str, point50LikeItems: str, pc_Replacement_8716_15_EA: str, pc_Replacement_Q667_600_EA: str, opts: Optional[dict] = None, query_method: bool = False):
+    def mbomStock(self, stockItem: libsimba.AssemblyBom.StockItem, specifiedSupplier: libsimba.AssemblyBom.Supplier, SOH: str, remanufacturedCost: str, totalCost: str, comment: str, point50LikeItems: str, pc_Replacement_8716_15_EA: str, pc_Replacement_Q667_600_EA: str, async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of mbomStock will be queried. Otherwise mbomStock will be invoked with inputs.
         """
@@ -38,9 +66,9 @@ class AssemblyBom:
         if query_method:
             return self.simba_contract.query_method("mbomStock", opts=opts)
         else:
-            return self.simba_contract.submit_method("mbomStock", inputs, opts=opts)
+            return self.simba_contract.submit_method("mbomStock", inputs, opts=opts, async_method=async_method)
 
-    def assemblage(self, name: dict, niins: List[dict], parts: List[dict], opts: Optional[dict] = None, query_method: bool = False):
+    def assemblage(self, name: libsimba.AssemblyBom.Assembly, niins: List[libsimba.AssemblyBom.Part], parts: List[libsimba.AssemblyBom.AssemblyPart], async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of assemblage will be queried. Otherwise assemblage will be invoked with inputs.
         """
@@ -52,9 +80,9 @@ class AssemblyBom:
         if query_method:
             return self.simba_contract.query_method("assemblage", opts=opts)
         else:
-            return self.simba_contract.submit_method("assemblage", inputs, opts=opts)
+            return self.simba_contract.submit_method("assemblage", inputs, opts=opts, async_method=async_method)
 
-    def assemblyGroup(self, name: dict, subAssemblies: List[dict], opts: Optional[dict] = None, query_method: bool = False):
+    def assemblyGroup(self, name: libsimba.AssemblyBom.Assembly, subAssemblies: List[libsimba.AssemblyBom.Assembly], async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of assemblyGroup will be queried. Otherwise assemblyGroup will be invoked with inputs.
         """
@@ -65,4 +93,4 @@ class AssemblyBom:
         if query_method:
             return self.simba_contract.query_method("assemblyGroup", opts=opts)
         else:
-            return self.simba_contract.submit_method("assemblyGroup", inputs, opts=opts)
+            return self.simba_contract.submit_method("assemblyGroup", inputs, opts=opts, async_method=async_method)
--- tests/app_md_contract.py
+++ tests/app_md_contract.py
@@ -11,6 +11,24 @@ class Application:
         self.simba = Simba(self.base_api_url)
         self.simba_contract = self.simba.get_contract(self.app_name, self.contract_name)
     
+    class Person:
+        def __init__(self, name: str = '2020-01-01T00:00:00.000', age: int = 99, addr: libsimba.Application.Addr = None):
+            self.name=name
+            self.age=age
+            self.addr=addr
+    
+    class Addr:
+        def __init__(self, street: str = '', number: int = 0, town: str = ''):
+            self.street=street
+            self.number=number
+            self.town=town
+    
+    class AddressPerson:
+        def __init__(self, name: str = '', age: int = 0, addrs: List[libsimba.Application.Addr] = []):
+            self.name=name
+            self.age=age
+            self.addrs=addrs
+    
     def get_transactions(self, opts: Optional[dict] = None):
         return self.simba_contract.get_transactions(opts=opts)
     
@@ -20,7 +38,7 @@ class Application:
     def get_transaction_statuses(self, txn_hashes: List[str] = None, opts: Optional[dict] = None):
         return self.simba_contract.get_transaction_statuses(txn_hashes, opts)
 
-    def a_str_arr(self, first: List[str], opts: Optional[dict] = None, query_method: bool = False):
+    def a_str_arr(self, first: List[str], async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of a_str_arr will be queried. Otherwise a_str_arr will be invoked with inputs.
         """
@@ -30,9 +48,9 @@ class Application:
         if query_method:
             return self.simba_contract.query_method("a_str_arr", opts=opts)
         else:
-            return self.simba_contract.submit_method("a_str_arr", inputs, opts=opts)
+            return self.simba_contract.submit_method("a_str_arr", inputs, opts=opts, async_method=async_method)
 
-    def an_arr(self, first: List[int], opts: Optional[dict] = None, query_method: bool = False):
+    def an_arr(self, first: List[int], async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of an_arr will be queried. Otherwise an_arr will be invoked with inputs.
         """
@@ -42,9 +60,9 @@ class Application:
         if query_method:
             return self.simba_contract.query_method("an_arr", opts=opts)
         else:
-            return self.simba_contract.submit_method("an_arr", inputs, opts=opts)
+            return self.simba_contract.submit_method("an_arr", inputs, opts=opts, async_method=async_method)
 
-    def two_arrs(self, first: List[int], second: List[int], opts: Optional[dict] = None, query_method: bool = False):
+    def two_arrs(self, first: List[int], second: List[int], async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of two_arrs will be queried. Otherwise two_arrs will be invoked with inputs.
         """
@@ -55,9 +73,9 @@ class Application:
         if query_method:
             return self.simba_contract.query_method("two_arrs", opts=opts)
         else:
-            return self.simba_contract.submit_method("two_arrs", inputs, opts=opts)
+            return self.simba_contract.submit_method("two_arrs", inputs, opts=opts, async_method=async_method)
 
-    def address_arr(self, first: List[str], opts: Optional[dict] = None, query_method: bool = False):
+    def address_arr(self, first: List[str], async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of address_arr will be queried. Otherwise address_arr will be invoked with inputs.
         """
@@ -67,9 +85,9 @@ class Application:
         if query_method:
             return self.simba_contract.query_method("address_arr", opts=opts)
         else:
-            return self.simba_contract.submit_method("address_arr", inputs, opts=opts)
+            return self.simba_contract.submit_method("address_arr", inputs, opts=opts, async_method=async_method)
 
-    def bbb(self, first: List[List[int]], opts: Optional[dict] = None, query_method: bool = False):
+    def bbb(self, first: List[List[int]], async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of bbb will be queried. Otherwise bbb will be invoked with inputs.
         """
@@ -79,9 +97,9 @@ class Application:
         if query_method:
             return self.simba_contract.query_method("bbb", opts=opts)
         else:
-            return self.simba_contract.submit_method("bbb", inputs, opts=opts)
+            return self.simba_contract.submit_method("bbb", inputs, opts=opts, async_method=async_method)
 
-    def nested_arr(self, first: List[List[int]], opts: Optional[dict] = None, query_method: bool = False):
+    def nested_arr(self, first: List[List[int]], async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of nested_arr will be queried. Otherwise nested_arr will be invoked with inputs.
         """
@@ -91,9 +109,9 @@ class Application:
         if query_method:
             return self.simba_contract.query_method("nested_arr", opts=opts)
         else:
-            return self.simba_contract.submit_method("nested_arr", inputs, opts=opts)
+            return self.simba_contract.submit_method("nested_arr", inputs, opts=opts, async_method=async_method)
 
-    def nested_arr_2(self, first: List[List[int]], opts: Optional[dict] = None, query_method: bool = False):
+    def nested_arr_2(self, first: List[List[int]], async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of nested_arr_2 will be queried. Otherwise nested_arr_2 will be invoked with inputs.
         """
@@ -103,9 +121,9 @@ class Application:
         if query_method:
             return self.simba_contract.query_method("nested_arr_2", opts=opts)
         else:
-            return self.simba_contract.submit_method("nested_arr_2", inputs, opts=opts)
+            return self.simba_contract.submit_method("nested_arr_2", inputs, opts=opts, async_method=async_method)
 
-    def nested_arr_3(self, first: List[List[int]], opts: Optional[dict] = None, query_method: bool = False):
+    def nested_arr_3(self, first: List[List[int]], async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of nested_arr_3 will be queried. Otherwise nested_arr_3 will be invoked with inputs.
         """
@@ -115,7 +133,7 @@ class Application:
         if query_method:
             return self.simba_contract.query_method("nested_arr_3", opts=opts)
         else:
-            return self.simba_contract.submit_method("nested_arr_3", inputs, opts=opts)
+            return self.simba_contract.submit_method("nested_arr_3", inputs, opts=opts, async_method=async_method)
 
     def nested_arr_4(self, first: List[List[int]], files: List[Tuple], async_method: bool = False, opts: Optional[dict] = None):
         """
@@ -129,7 +147,7 @@ class Application:
         else:
             return self.simba_contract.submit_contract_method_with_files("nested_arr_4", inputs, files=files, opts=opts)
 
-    def structTest_1(self, people: List[dict], test_bool: bool, opts: Optional[dict] = None, query_method: bool = False):
+    def structTest_1(self, people: List[libsimba.Application.Person], test_bool: bool, async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of structTest_1 will be queried. Otherwise structTest_1 will be invoked with inputs.
         """
@@ -140,9 +158,9 @@ class Application:
         if query_method:
             return self.simba_contract.query_method("structTest_1", opts=opts)
         else:
-            return self.simba_contract.submit_method("structTest_1", inputs, opts=opts)
+            return self.simba_contract.submit_method("structTest_1", inputs, opts=opts, async_method=async_method)
 
-    def structTest_2(self, person: dict, test_bool: bool, opts: Optional[dict] = None, query_method: bool = False):
+    def structTest_2(self, person: libsimba.Application.Person, test_bool: bool, async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of structTest_2 will be queried. Otherwise structTest_2 will be invoked with inputs.
         """
@@ -153,9 +171,9 @@ class Application:
         if query_method:
             return self.simba_contract.query_method("structTest_2", opts=opts)
         else:
-            return self.simba_contract.submit_method("structTest_2", inputs, opts=opts)
+            return self.simba_contract.submit_method("structTest_2", inputs, opts=opts, async_method=async_method)
 
-    def structTest_5(self, person: dict, files: List[Tuple], async_method: bool = False, opts: Optional[dict] = None):
+    def structTest_5(self, person: libsimba.Application.Person, files: List[Tuple], async_method: bool = False, opts: Optional[dict] = None):
         """
         If async_method == True, then structTest_5 will be invoked as async, otherwise structTest_5 will be invoked as non async
         """
@@ -167,7 +185,7 @@ class Application:
         else:
             return self.simba_contract.submit_contract_method_with_files("structTest_5", inputs, files=files, opts=opts)
 
-    def structTest_3(self, person: dict, files: List[Tuple], async_method: bool = False, opts: Optional[dict] = None):
+    def structTest_3(self, person: libsimba.Application.AddressPerson, files: List[Tuple], async_method: bool = False, opts: Optional[dict] = None):
         """
         If async_method == True, then structTest_3 will be invoked as async, otherwise structTest_3 will be invoked as non async
         """
@@ -179,7 +197,7 @@ class Application:
         else:
             return self.simba_contract.submit_contract_method_with_files("structTest_3", inputs, files=files, opts=opts)
 
-    def structTest_4(self, persons: List[dict], files: List[Tuple], async_method: bool = False, opts: Optional[dict] = None):
+    def structTest_4(self, persons: List[libsimba.Application.AddressPerson], files: List[Tuple], async_method: bool = False, opts: Optional[dict] = None):
         """
         If async_method == True, then structTest_4 will be invoked as async, otherwise structTest_4 will be invoked as non async
         """
@@ -191,7 +209,7 @@ class Application:
         else:
             return self.simba_contract.submit_contract_method_with_files("structTest_4", inputs, files=files, opts=opts)
 
-    def structTest_6(self, persons_0: List[dict], files: List[Tuple], async_method: bool = False, opts: Optional[dict] = None):
+    def structTest_6(self, persons_0: List[libsimba.Application.AddressPerson], files: List[Tuple], async_method: bool = False, opts: Optional[dict] = None):
         """
         If async_method == True, then structTest_6 will be invoked as async, otherwise structTest_6 will be invoked as non async
         """
@@ -203,7 +221,7 @@ class Application:
         else:
             return self.simba_contract.submit_contract_method_with_files("structTest_6", inputs, files=files, opts=opts)
 
-    def nowt(self, opts: Optional[dict] = None, query_method: bool = False):
+    def nowt(self, async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of nowt will be queried. Otherwise nowt will be invoked with inputs.
         """
@@ -212,9 +230,9 @@ class Application:
         if query_method:
             return self.simba_contract.query_method("nowt", opts=opts)
         else:
-            return self.simba_contract.submit_method("nowt", inputs, opts=opts)
+            return self.simba_contract.submit_method("nowt", inputs, opts=opts, async_method=async_method)
 
-    def date_string(self, the_date: str, opts: Optional[dict] = None, query_method: bool = False):
+    def date_string(self, the_date: str, async_method: bool = False, opts: Optional[dict] = None, query_method: bool = False):
         """
         If query_method == True, then invocations of date_string will be queried. Otherwise date_string will be invoked with inputs.
         """
@@ -224,4 +242,4 @@ class Application:
         if query_method:
             return self.simba_contract.query_method("date_string", opts=opts)
         else:
-            return self.simba_contract.submit_method("date_string", inputs, opts=opts)
+            return self.simba_contract.submit_method("date_string", inputs, opts=opts, async_method=async_method)
